66c1d6d401a0128b81725b2811cfeb481b585858
Dev Verma <devv6687@gmail.com>
2026-01-30 12:08:52 +0530

    Create 30jan2026.R

---
+++ 30jan2026.R
@@ -0,0 +1,25 @@
+# 1. rogram to create a vector of first 50 even numbers
+v1 <- seq(2,50,2)
+v1
+# 2. program to create a vector of first 50 odd numbers
+v2 <- seq(1,50,2)
+v2
+# 3. create a vector with 12 integers and convert it into a 4*3 MATRIX using a matrix function
+m1 <- matrix(1:12, nrow = 4, ncol = 3)
+m1
+# 4. create a vector of pattern 1,2---20 then 19,18--1.
+v3 <- seq(1,20,1)
+v 
+# 5. write a r prpogram to divide two vectors
+# 6. write a r program to reverse order of the given vector
+# 7. r program to add delete and modify at particular index  the elements in a vector
+# 8. write a program to generate 15 random numbers and find the numbers which are greater than average 
+# 9. write an r program to perform the following operations on a vector: 
+# a) find sum
+# b) find mean
+# c) find max and min values
+# d) add , subtract , multiply and divide the vectors
+# 10. create a vector of colours of book colour and print its class 
+# 11. create 3 vectors : vector 1 : emp_id(1,2,3,4) ; vector 2 : emp_name(dev ,shweta, raghav, vishal) ; vector 3 : total employee = 4 ; 
+# now create a list containing these 3 vectors and print them
+
